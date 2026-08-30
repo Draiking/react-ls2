be5e1ab3f42ef5127319c958a2d552b244cb5ee3
Draiking <heavyyrain3039@gmail.com>
2026-08-30 15:30:15 +0300

    footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Techologies from './Techologies';
 import Headers from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Headers />
       <Techologies />
+      <Footer />
     </div>
   );
 }
