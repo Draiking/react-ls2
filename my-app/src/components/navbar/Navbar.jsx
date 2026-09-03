36a037ca6df53357b57bef36fe980f0b90a2a313
Draiking <heavyyrain3039@gmail.com>
2026-09-04 01:41:36 +0300

    href

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -4,19 +4,19 @@ import stylles from './Navbar.module.css';
 const NavBar = () => {
   return <nav className={stylles.nav}>
     <div className={stylles.item}>
-      <a>Profile</a>
+      <a href='/profile'>Profile</a>
     </div>
     <div className={`${stylles.item} ${stylles.active}`}>
-      <a>Messages</a>
+      <a href='/messages'>Messages</a>
     </div>
     <div className={stylles.item}>
-      <a>News</a>
+      <a href='/news'>News</a>
     </div>
     <div className={stylles.item}>
-      <a>Music</a>
+      <a href='/music'>Music</a>
     </div>
     <div className={stylles.item}>
-      <a>Settings</a>
+      <a href='/setting'>Settings</a>
     </div>
   </nav>
 }
