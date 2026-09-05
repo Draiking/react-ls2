ab93f7f2d1211dfd73360d02e34b8fb1fb5e2244
Draiking <heavyyrain3039@gmail.com>
2026-09-05 16:09:53 +0300

    navLink - stylles

--- my-app/src/components/navbar/Navbar.jsx
+++ my-app/src/components/navbar/Navbar.jsx
@@ -5,19 +5,34 @@ import stylles from './Navbar.module.css';
 const NavBar = () => {
   return <nav className={stylles.nav}>
     <div className={stylles.item}>
-      <NavLink to='/profile'>Profile</NavLink>
+      <NavLink 
+      to='/profile' 
+      className={({ isActive }) => isActive ? stylles.activeLink : ''
+      }>Profile</NavLink>
     </div>
-    <div className={`${stylles.item} ${stylles.active}`}>
-      <NavLink to='/messages'>Messages</NavLink>
+    <div className={`${stylles.item}`}>
+      <NavLink 
+      to='/messages' 
+      className={({isActive}) => isActive ? stylles.activeLink : ''}
+      >Messages</NavLink>
     </div>
     <div className={stylles.item}>
-      <NavLink to='/news'>News</NavLink>
+      <NavLink 
+      to='/news'
+      className={({isActive}) => isActive ? stylles.activeLink : ''}
+      >News</NavLink>
     </div>
     <div className={stylles.item}>
-      <NavLink to='/music'>Music</NavLink>
+      <NavLink 
+      to='/music'
+      className={({isActive}) => isActive ? stylles.activeLink : ''}
+      >Music</NavLink>
     </div>
     <div className={stylles.item}>
-      <NavLink to='/settings'>Settings</NavLink>
+      <NavLink 
+      to='/settings'
+      className={({isActive}) => isActive ? stylles.activeLink : ''}
+      >Settings</NavLink>
     </div>
   </nav>
 }
