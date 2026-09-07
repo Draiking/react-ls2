b62f4849f92e324f967d0625278744d642e22ec4
Draiking <heavyyrain3039@gmail.com>
2026-09-07 13:46:49 +0300

    css dialog

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,31 @@ import stylles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div className={stylles.content}>
-            Dialogs
+        <div className={stylles.dialogs}>
+            <div className={stylles.dialogsItems}>
+                <div className={stylles.dialog + ' ' + stylles.active}>
+                    Dimych
+                </div>
+                <div className={stylles.dialog}>
+                    Sveta
+                </div>
+                <div className={stylles.dialog}>
+                    Andrey
+                </div>
+                <div className={stylles.dialog}>
+                    Sasha
+                </div>
+                <div className={stylles.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={stylles.messages}>
+                <div className={stylles.message}>hi</div>
+                <div className={stylles.message}>how are you</div>
+                <div className={stylles.message}>word</div>
+            </div>
         </div>
+
     )
 }
 
