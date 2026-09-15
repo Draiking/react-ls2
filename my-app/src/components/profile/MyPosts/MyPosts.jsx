a0fdaf8af3c3b47dae646fc05d24182fa27bb881
Draiking <heavyyrain3039@gmail.com>
2026-09-15 15:14:43 +0300

    useRef

--- my-app/src/components/profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/profile/MyPosts/MyPosts.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import stylles from './MyPosts.module.css';
 import Post from './Post/Post';
 
@@ -5,14 +6,21 @@ const MyPosts = (props) => {
   let postsElements =
     props.postsData.map(p => <Post key={p.id} message={p.message} likecount={p.likecount} />)
 
+  let newPostElement = useRef(null);
+
+  let addPost = () => {
+    let text = newPostElement.current.value
+    alert(text)
+  }  
+
   return <div className={stylles.postBlock}>
     <h3>My posts</h3>
     <div>
       <div>
-        <textarea></textarea>
+        <textarea ref={newPostElement}></textarea>
       </div>
       <div>
-        <button>add post</button>
+        <button onClick={addPost}>add post</button>
       </div>
     </div>
     <div className={stylles.posts}>
